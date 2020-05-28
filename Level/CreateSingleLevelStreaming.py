4eab0d059436c0b07576f84eed60a4e4ca3db21b
Muhammad A.Moniem <muhammadamoniem@gmail.com>
2020-05-27 20:37:25 -0400

    - add level streaming creation example

---
+++ Level/CreateSingleLevelStreaming.py
@@ -0,0 +1,18 @@
+#										_
+#									   (_)
+#  _ __ ___   __ _ _ __ ___   ___  _ __  _  ___ _ __ ___
+# | '_ ` _ \ / _` | '_ ` _ \ / _ \| '_ \| |/ _ \ '_ ` _ \
+# | | | | | | (_| | | | | | | (_) | | | | |  __/ | | | | |
+# |_| |_| |_|\__,_|_| |_| |_|\___/|_| |_|_|\___|_| |_| |_|
+#					www.mamoniem.com
+#					  www.ue4u.xyz
+#Copyright 2020 Muhammad A.Moniem (@_mamoniem). All Rights Reserved.
+#
+
+import unreal
+
+@unreal.uclass()
+class MyEditorLevelUtility(unreal.EditorLevelUtils):
+    pass
+
+streamingLvl = MyEditorLevelUtility().create_new_streaming_level(unreal.LevelStreamingDynamic, "/Game/Streaming/lvl_TestStreaming", False)
