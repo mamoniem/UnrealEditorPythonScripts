603eb791ebbd33635f65b25cce6f19f0f7ec7899
Muhammad A.Moniem <muhammadamoniem@gmail.com>
2021-05-27 12:23:30 -0400

    - add the report two sided materials example

---
+++ Materials/ReportTwoSidedMaterials.py
@@ -0,0 +1,37 @@
+#										_
+#									   (_)
+#  _ __ ___   __ _ _ __ ___   ___  _ __  _  ___ _ __ ___
+# | '_ ` _ \ / _` | '_ ` _ \ / _ \| '_ \| |/ _ \ '_ ` _ \
+# | | | | | | (_| | | | | | | (_) | | | | |  __/ | | | | |
+# |_| |_| |_|\__,_|_| |_| |_|\___/|_| |_|_|\___|_| |_| |_|
+#					www.mamoniem.com
+#					  www.ue4u.xyz
+#Copyright 2021 Muhammad A.Moniem (@_mamoniem). All Rights Reserved.
+#
+
+import unreal
+
+workingPath = "/Game/"
+
+@unreal.uclass()
+class GetEditorAssetLibrary(unreal.EditorAssetLibrary):
+    pass
+
+editorAssetLib = GetEditorAssetLibrary()
+
+allAssets = editorAssetLib.list_assets(workingPath, True, False)
+
+if (len(allAssets) > 0):
+    for asset in allAssets:
+        assetData = editorAssetLib.find_asset_data(asset)
+        if(assetData.get_asset().get_class().get_name() == "Material"):
+            if(assetData.get_asset().get_editor_property("two_sided") == True):
+                unreal.log_warning("Master material [%s] is TWO-SIDED" % assetData.asset_name)
+            else:
+                unreal.log("Master material [%s] OK" % assetData.asset_name)
+        elif (assetData.get_asset().get_class().get_name() == "MaterialInstance" or assetData.get_asset().get_class().get_name() == "MaterialInstanceConstant"):
+            baseOverrides = assetData.get_asset().get_editor_property("base_property_overrides")
+            if (baseOverrides.get_editor_property("override_two_sided") == True):
+                unreal.log_warning("Material Instance [%s] is TWO-SIDED" % assetData.asset_name)
+            else:
+                unreal.log("Material Instance [%s] OK" % assetData.asset_name)
