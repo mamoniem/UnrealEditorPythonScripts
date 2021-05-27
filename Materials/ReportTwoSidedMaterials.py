9943c7ddb4b6e4706e546fd700b7bf8d43579da3
Muhammad A.Moniem <muhammadamoniem@gmail.com>
2021-05-27 12:34:39 -0400

    - update the two-sided reporting example with scoped slow tasks

--- Materials/ReportTwoSidedMaterials.py
+++ Materials/ReportTwoSidedMaterials.py
@@ -20,18 +20,27 @@ class GetEditorAssetLibrary(unreal.EditorAssetLibrary):
 editorAssetLib = GetEditorAssetLibrary()
 
 allAssets = editorAssetLib.list_assets(workingPath, True, False)
+allAssetsCount = len(allAssets)
+selectedAssetPath = "/Content"
 
-if (len(allAssets) > 0):
-    for asset in allAssets:
-        assetData = editorAssetLib.find_asset_data(asset)
-        if(assetData.get_asset().get_class().get_name() == "Material"):
-            if(assetData.get_asset().get_editor_property("two_sided") == True):
-                unreal.log_warning("Master material [%s] is TWO-SIDED" % assetData.asset_name)
-            else:
-                unreal.log("Master material [%s] OK" % assetData.asset_name)
-        elif (assetData.get_asset().get_class().get_name() == "MaterialInstance" or assetData.get_asset().get_class().get_name() == "MaterialInstanceConstant"):
-            baseOverrides = assetData.get_asset().get_editor_property("base_property_overrides")
-            if (baseOverrides.get_editor_property("override_two_sided") == True):
-                unreal.log_warning("Material Instance [%s] is TWO-SIDED" % assetData.asset_name)
-            else:
-                unreal.log("Material Instance [%s] OK" % assetData.asset_name)
+if (allAssetsCount > 0):
+    with unreal.ScopedSlowTask(allAssetsCount, selectedAssetPath) as slowTask:
+        slowTask.make_dialog(True)
+        for asset in allAssets:
+            assetData = editorAssetLib.find_asset_data(asset)
+            selectedAssetPath = assetData.get_asset().get_path_name()
+            if(assetData.get_asset().get_class().get_name() == "Material"):
+                if(assetData.get_asset().get_editor_property("two_sided") == True):
+                    unreal.log_warning("Master material [%s] is TWO-SIDED" % assetData.asset_name)
+                else:
+                    unreal.log("Master material [%s] OK" % assetData.asset_name)
+            elif (assetData.get_asset().get_class().get_name() == "MaterialInstance" or assetData.get_asset().get_class().get_name() == "MaterialInstanceConstant"):
+                baseOverrides = assetData.get_asset().get_editor_property("base_property_overrides")
+                if (baseOverrides.get_editor_property("override_two_sided") == True):
+                    unreal.log_warning("Material Instance [%s] is TWO-SIDED" % assetData.asset_name)
+                else:
+                    unreal.log("Material Instance [%s] OK" % assetData.asset_name)
+
+                if slowTask.should_cancel():
+                    break
+                slowTask.enter_progress_frame(1, asset)
