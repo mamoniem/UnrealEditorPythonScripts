6198da2d25b4e5c063488454e742c8256e26c42e
Muhammad A.Moniem <muhammadamoniem@gmail.com>
2021-05-27 13:33:44 -0400

    - make the two sided reporter example compatible with UE4.26 & UE4.27 - expand the logging to give better summary at the end for the two sided reporter example

--- Materials/ReportTwoSidedMaterials.py
+++ Materials/ReportTwoSidedMaterials.py
@@ -21,7 +21,9 @@ editorAssetLib = GetEditorAssetLibrary()
 
 allAssets = editorAssetLib.list_assets(workingPath, True, False)
 allAssetsCount = len(allAssets)
-selectedAssetPath = "/Content"
+selectedAssetPath = workingPath
+mastersWithTwoSided = 0
+instancesWithTwoSided = 0
 
 if (allAssetsCount > 0):
     with unreal.ScopedSlowTask(allAssetsCount, selectedAssetPath) as slowTask:
@@ -29,18 +31,22 @@ if (allAssetsCount > 0):
         for asset in allAssets:
             assetData = editorAssetLib.find_asset_data(asset)
             selectedAssetPath = assetData.get_asset().get_path_name()
-            if(assetData.get_asset().get_class().get_name() == "Material"):
+            if(assetData.asset_class == "Material"):
                 if(assetData.get_asset().get_editor_property("two_sided") == True):
                     unreal.log_warning("Master material [%s] is TWO-SIDED" % assetData.asset_name)
+                    mastersWithTwoSided += 1
                 else:
                     unreal.log("Master material [%s] OK" % assetData.asset_name)
-            elif (assetData.get_asset().get_class().get_name() == "MaterialInstance" or assetData.get_asset().get_class().get_name() == "MaterialInstanceConstant"):
+            elif (assetData.asset_class == "MaterialInstance" or assetData.asset_class == "MaterialInstanceConstant"):
                 baseOverrides = assetData.get_asset().get_editor_property("base_property_overrides")
                 if (baseOverrides.get_editor_property("override_two_sided") == True):
                     unreal.log_warning("Material Instance [%s] is TWO-SIDED" % assetData.asset_name)
+                    instancesWithTwoSided += 1
                 else:
                     unreal.log("Material Instance [%s] OK" % assetData.asset_name)
 
                 if slowTask.should_cancel():
                     break
                 slowTask.enter_progress_frame(1, asset)
+
+    unreal.log_warning("Found [%d] Master Materials && [%d] Material Instance that hast the Two-Sided flag" % (mastersWithTwoSided, instancesWithTwoSided))
