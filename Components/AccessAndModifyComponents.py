5a2ffa8a3656f12dc88b19feddcee29cd83f5427
Muhammad A.Moniem <muhammadamoniem@gmail.com>
2021-07-28 04:55:20 -0400

    - add example of accessAndModifyComponents of selected actor[s]

---
+++ Components/AccessAndModifyComponents.py
@@ -0,0 +1,39 @@
+#										_
+#									   (_)
+#  _ __ ___   __ _ _ __ ___   ___  _ __  _  ___ _ __ ___
+# | '_ ` _ \ / _` | '_ ` _ \ / _ \| '_ \| |/ _ \ '_ ` _ \
+# | | | | | | (_| | | | | | | (_) | | | | |  __/ | | | | |
+# |_| |_| |_|\__,_|_| |_| |_|\___/|_| |_|_|\___|_| |_| |_|
+#					www.mamoniem.com
+#					  www.ue4u.xyz
+#Copyright 2021 Muhammad A.Moniem (@_mamoniem). All Rights Reserved.
+#
+
+#notice that this version affects only components that are (not) inherited. This is possibly due to unreal bug!
+#inherited components usually have a blue icon, where none-inherited have gray icon
+
+import unreal
+
+@unreal.uclass()
+class MyEditorUtility(unreal.GlobalEditorUtilityBase):
+    pass
+
+selectedActors = MyEditorUtility().get_selection_set()
+
+for actor in selectedActors:
+    unreal.log(actor.get_name())
+    unreal.log("*************************************************")
+
+    SMCompts = actor.root_component.get_children_components(True)
+    comptsCount = len(SMCompts)
+    print("Found [%d] components attached to the root"%(comptsCount))
+    for SMCompt in SMCompts:
+        print ("Checking compontnt [%s] which is [%s] if StaticMeshComponent"%(SMCompt.get_name(), SMCompt.get_class().get_name()))
+        if (SMCompt.get_class().get_name() == "StaticMeshComponent"):
+            print(">>TRUE")
+            SMComptT = SMCompt.relative_location
+            print(SMComptT)
+            SMCompt.set_relative_location(unreal.Vector(0.0, 0.0, 0.0), False, True)
+            print(SMComptT)
+        else:
+            print(">>FALSE")
---
+++ Components/AccessAndModifyComponentsNew.py
@@ -0,0 +1,32 @@
+#										_
+#									   (_)
+#  _ __ ___   __ _ _ __ ___   ___  _ __  _  ___ _ __ ___
+# | '_ ` _ \ / _` | '_ ` _ \ / _ \| '_ \| |/ _ \ '_ ` _ \
+# | | | | | | (_| | | | | | | (_) | | | | |  __/ | | | | |
+# |_| |_| |_|\__,_|_| |_| |_|\___/|_| |_|_|\___|_| |_| |_|
+#					www.mamoniem.com
+#					  www.ue4u.xyz
+#Copyright 2021 Muhammad A.Moniem (@_mamoniem). All Rights Reserved.
+#
+
+#notice that this version works with both type of blueprint/actor components inherited & none-inherited.
+#inherited components usually have a blue icon, where none-inherited have gray icon
+
+import unreal
+
+@unreal.uclass()
+class MyEditorUtility(unreal.GlobalEditorUtilityBase):
+    pass
+
+selectedActors = MyEditorUtility().get_selection_set()
+
+for actor in selectedActors:
+    unreal.log(actor.get_name())
+    unreal.log("****************************************************")
+
+    SMCompts = actor.get_components_by_class(unreal.StaticMeshComponent)
+    for SMCompt in SMCompts:
+        SMComptT = SMCompt.get_editor_property("relative_location")
+        print(SMComptT)
+        SMCompt.set_editor_property("relative_location", unreal.Vector(0.0, 0.0, 0.0))
+        print(SMComptT)
